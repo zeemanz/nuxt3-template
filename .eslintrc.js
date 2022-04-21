2b63e314e928f29d878882d2dba79413d66d88cf
Zeeman Zhang <cn.zeeman.zhang@gmail.com>
2022-04-21 17:27:04 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:nuxt/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
