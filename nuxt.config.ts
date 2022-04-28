958bc93ce612b6edbfb7cf5c2996e8a008d6c498
Zeeman Zhang <cn.zeeman.zhang@gmail.com>
2022-04-28 15:47:43 +0800

    chore: Integrate Element Plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 import { defineNuxtConfig } from 'nuxt';
 
+const lifecycle = process.env.npm_lifecycle_event;
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  build: {
+    transpile:
+      lifecycle === 'build' || lifecycle === 'generate' ? ['element-plus'] : [],
+  },
+  css: ['~/assets/scss/element-plus.scss'],
+});
